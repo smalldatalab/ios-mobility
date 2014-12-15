24bd2ab797618a72b587f8b27e22e004d2c0f3d6
Charlie Forkish <cforkish@gmail.com>
2014-12-15 14:20:18 +0700

    Initial Commit

---
+++ Mobility/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mobility
+//
+//  Created by Charles Forkish on 12/15/14.
+//  Copyright (c) 2014 Open mHealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
