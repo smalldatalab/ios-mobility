97ff6cae4d3de79b695f172d53cc7cedab5e040d
Charlie Forkish <cforkish@gmail.com>
2015-02-17 09:38:31 +0700

    updated to new bundle ID: io.smalldatalab.mobility, and updated google client ID and URL scheme accordingly

--- Mobility/AppConstants.m
+++ Mobility/AppConstants.m
@@ -9,7 +9,7 @@
 #import "AppConstants.h"
 
 
-NSString * const kMobilityGoogleClientID = @"48636836762-ba1jcrir6sft063gkvpav0e3o9p4mtb5.apps.googleusercontent.com";
+NSString * const kMobilityGoogleClientID = @"48636836762-bt7sitfa4fk4h41i0rsq0tf9jdc9qrud.apps.googleusercontent.com";
 NSString * const kOMHServerGoogleClientID = @"48636836762-mulldgpmet2r4s3f16s931ea9crcc64m.apps.googleusercontent.com";
 NSString * const kMobilityDSUClientID = @"org.openmhealth.ios.mobility";
 NSString * const kMobilityDSUClientSecret = @"Rtg43jkLD7z76c";
