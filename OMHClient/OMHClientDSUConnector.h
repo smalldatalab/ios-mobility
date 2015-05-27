c3d0c72749807faa8c6e3a8a99a656514bc7c524
Charlie Forkish <cforkish@gmail.com>
2015-05-27 10:33:32 +0800

    removed and re-added Crashlytics/Fabric frameworks to address "missing framework" build error.

--- OMHClient/OMHClientDSUConnector.h
+++ OMHClient/OMHClientDSUConnector.h
@@ -72,12 +72,15 @@ sourceApplication:(NSString *)sourceApplication
 
 @protocol OMHSignInDelegate
 
-- (void)OMHClient:(OMHClient *)client signInFinishedWithError:(NSError *)error;
-- (void)OMHClientUserWasSignedOut:(OMHClient *)client;
-
 // we need these for presenting the google+ sign in web view
 - (void)presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion;
 - (void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion;
+
+- (void)OMHClient:(OMHClient *)client signInFinishedWithError:(NSError *)error;
+
+@optional
+- (void)OMHClientUserWasSignedOut:(OMHClient *)client;
+
 
 @end
 
